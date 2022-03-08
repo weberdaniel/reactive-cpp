ca356bbed3fc2cf21fdc02d40b7b93050ede8db6
weberdaniel <48121307+weberdaniel@users.noreply.github.com>
2022-03-08 08:56:10 +0100

    added restart_strategy string_view to supervisor_state in order to resemble the restart strategies known from erlang

--- app/svdemo/supervisor.h
+++ app/svdemo/supervisor.h
@@ -15,21 +15,22 @@ struct supervisor_state  {
   std::vector<actor> children;
   // restart_strategy: assign with type_name<simple_one_for_one>::value;
   caf::string_view restart_strategy;
+  // how often are we going to restart during a specific period
+  int32_t intensity;
+  std::chrono::microseconds period;
 };
 
 behavior supervisor(stateful_actor<supervisor_state>* self, const actor& buddy) {
-
-    auto v = caf::put_atom_v;
-    self->send(buddy,rest_for_one_v);
-    aout(self) << "Supervisor started (Id " << self->id() << ")" << std::endl;
-    self->home_system().registry().put("sv", self);
-    self->monitor(buddy);
-    actor_id mon_id = buddy->id();
-    aout(self) << "Supervisor (Id " << self->id() << ") monitors Actor (Id " << mon_id << ")" << std::endl;
-    self->set_down_handler([=](down_msg&) {
-      //self->home_system()->spawn();
-    });
+  auto v = caf::put_atom_v;
+  aout(self) << "Supervisor started (Id " << self->id() << ")" << std::endl;
+  self->home_system().registry().put("sv", self);
+  self->monitor(buddy);
+  actor_id mon_id = buddy->id();
+  aout(self) << "Supervisor (Id " << self->id() << ") monitors Actor (Id " << mon_id << ")" << std::endl;
+  self->set_down_handler([=](down_msg&) {
+    aout(self) << "down" << std::endl;
+  });
   return {
-    [=](int32_t val) { self->state.svid= val; }
+    [=](int32_t val) { self->state.restart_strategy = type_name<simple_one_for_one>::value; }
   };
 }

a2ec291a2b6f87c9ff253bb04d1339bdb0f98b26
weberdaniel <48121307+weberdaniel@users.noreply.github.com>
2022-05-11 13:42:44 +0200

    addes message type and inspector, added name of the process and an instance id. next arrange actors in a circle.

--- app/svdemo/main.cpp
+++ app/svdemo/main.cpp
@@ -7,19 +7,38 @@
 
 using namespace caf;
 
+struct data {
+  int source;
+  int destination;
+  std::string content;
+};
+
+template <class Inspector>
+bool inspect(Inspector& f, data x) {
+  return f.object(x).fields(
+    f.field("source", x.source),
+    f.field("destination", x.destination),
+    f.field("content", x.content));
+}
+
 struct worker_static_state {
-    int delay;
+  int instance_id;
+  std::string name;
+
+  int delay;
 };
 
 struct worker_dynamic_state {
-    int counter;
+  int counter;
 };
 
 class worker {
 public:
-  explicit worker(int tst) {
+  explicit worker(std::string name, int instance_id, int delay) {
     static_state = std::make_shared<worker_static_state>();
-    static_state->delay = tst;
+    static_state->instance_id = instance_id;
+    static_state->delay = delay;
+    static_state->name = name;
   }
 
   worker(const worker& ref) {
@@ -32,20 +51,27 @@ public:
     std::shared_ptr<worker_static_state> config = static_state;
     std::shared_ptr<worker_dynamic_state> state = std::make_shared<worker_dynamic_state>();
 
-    working.assign([config,state,self](int a) {
+    working.assign(
+      [config,state,self](int a) {
         state->counter++;
         if( state->counter == 3) {
-            throw std::bad_alloc();
+          throw std::bad_alloc();
         }
         aout(self) << "ongoing\n";
         self->delayed_send(self, std::chrono::seconds(config->delay), 3);
-    });
+      },
+      [config,state,self]() {
+
+      }
+    );
+
     self->delayed_send(self, std::chrono::seconds(config->delay), 3);
+    self->home_system().registry().put(config->name,self);
     self->become(working);
   }
 
   void operator()(event_based_actor* self) {
-      init(self);
+    init(self);
   }
 
   behavior working;
@@ -53,24 +79,23 @@ public:
 };
 
 void application(event_based_actor* self) {
-    supervisor supervisor;
-    worker worker(2);
+  supervisor supervisor;
+  worker worker( "circle", 0, 2);
 
-    childspec child_specification;
-    child_specification.child_id = "worker";
-    child_specification.start = worker;
+  childspec child_specification;
+  child_specification.child_id = "worker";
+  child_specification.start = worker;
 
-    std::vector<childspec> childspec_list;
-    childspec_list.push_back(child_specification);
+  std::vector<childspec> childspec_list;
+  childspec_list.push_back(child_specification);
 
-    supervisor.init(childspec_list);
-    self->home_system().spawn(supervisor);
+  supervisor.init(childspec_list);
+  self->home_system().spawn(supervisor);
 };
 
-
 void caf_main(actor_system& system) {
-    system.spawn(application);
-    system.await_actors_before_shutdown();
+  system.spawn(application);
+  system.await_actors_before_shutdown();
 }
 
 CAF_MAIN(id_block::supervisor)

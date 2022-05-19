6726500c69221adf97e5f04911ab4411a9a743f7
weberdaniel <48121307+weberdaniel@users.noreply.github.com>
2022-05-19 20:20:59 +0200

    refactor

--- app/svdemo/main.cpp
+++ app/svdemo/main.cpp
@@ -8,24 +8,23 @@
 
 using namespace caf;
 
-void application(event_based_actor* self) {
-  supervisor supervisor;
-  int n = 5;
-
-  // start n workers and let them be supervised by a central supervisor
-  std::vector<childspec> childspec_list;
-  // i = 0 is invalid id by defintion in messages.h
+std::vector<childspec> init_workers(int n) {
+  std::vector<childspec> children;
   for( int i = 1; i <= n; i++) {
     childspec child_specification;
-    child_specification.child_id =
-      std::string("worker") + "_" + std::to_string(i);
-    child_specification.start =
-      std::move(worker("worker",i,
-      std::chrono::milliseconds(100)) );
-    childspec_list.push_back(std::move(child_specification));
+    child_specification.child_id = std::string("worker_") + std::to_string(i);
+    child_specification.start = std::move(
+      worker("worker",i, std::chrono::milliseconds(100))
+    );
+    children.push_back(std::move(child_specification));
   }
+  return children;
+};
 
-  supervisor.init(childspec_list);
+void application(event_based_actor* self) {
+  supervisor supervisor;
+  std::vector<childspec> child_specifications = init_workers(5);
+  supervisor.init(child_specifications);
   self->home_system().spawn(supervisor);
 };
 
